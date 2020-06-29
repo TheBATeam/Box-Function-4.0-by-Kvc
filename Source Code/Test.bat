392218bf5d01ca2ecadb273834d96a51bd69a8c7
Ankitamehra93 <48797537+Ankitamehra93@users.noreply.github.com>
2020-06-29 20:01:25 +0530

    First Commit

---
+++ Source Code/Test.bat
@@ -0,0 +1,14 @@
+@Echo off
+Cls
+Cd Files
+Title Box Function 4.0 - DEMO - www.thebateam.org
+
+Echo. If You can see the Diffrence Between Diffrent Boxes (Border Styles!)
+
+Call Box 2 2 10 20 - - 2a 0
+Call Box 24 2 10 20 - - 2a 1
+Call Box 2 14 10 20 - - 2a 2
+Call Box 24 14 10 20 - - 2a 3
+Call Box 48 9 10 20 - - 2a 4
+
+pause >nul
